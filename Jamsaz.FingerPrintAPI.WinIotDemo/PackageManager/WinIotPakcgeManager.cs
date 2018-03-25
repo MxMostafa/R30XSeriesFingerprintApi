1fb9c509f582ca2398b8ed101dd3af872f5964f1
MxMostafa <bgr.mostafa@gmail.com>
2018-03-25 12:07:30 +0430

    Update WinIotPakcgeManager.cs

--- Jamsaz.FingerPrintAPI.WinIotDemo/PackageManager/WinIotPakcgeManager.cs
+++ Jamsaz.FingerPrintAPI.WinIotDemo/PackageManager/WinIotPakcgeManager.cs
@@ -28,7 +28,7 @@ namespace Jamsaz.FingerPrintAPI.WinIotDemo.PackageManager
                 _packetRecived = new AutoResetEvent(false);
                 var deviceSelector = SerialDevice.GetDeviceSelector();
                 var devices = DeviceInformation.FindAllAsync(deviceSelector).AsTask().Result;
-
+فثسف
                 if (devices.Count <= 0)
                 {
                     throw new Exception("Device not found -->[WinIotPakcgeManager_Constructor]");
